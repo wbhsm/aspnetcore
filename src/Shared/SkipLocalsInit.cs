840a89ec403e17e48bb3a799ac3124fd92df9a5d
James Newton-King <james@newtonking.com>
2020-10-05 13:33:12 +1300

    Add SkipLocalsInit

---
+++ src/Shared/SkipLocalsInit.cs
@@ -0,0 +1,8 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+#nullable disable
+// Used to indicate to the compiler that the .locals init flag should not be set in method headers.
+#if NET5_0
+[module: System.Runtime.CompilerServices.SkipLocalsInit]
+#endif
